10b3227c6d2a91bb0992b3b6ec69fc06bc40d9f8
root <root@vmi571387.contaboserver.net>
2022-03-10 01:12:05 +0100

    standardize error, grace crash, aihoster division

--- main.js
+++ main.js
@@ -13,74 +13,89 @@ const dntpCommunicator =
   new DntpCommunicator(config.dntpServerAddr, config.localMapDir);
 
 const rooms={};
-
+try{
 // plasmid to mgr event listener
-eventEmitter.on('plasmidRequest', async (requestDict)=>{
-  const action=requestDict.action;
-  const parameters=requestDict.parameters;
-  switch (action) {
-    case 'startGame':
-      // check if map exists
-      const mapQuery = await dntpCommunicator.getMapUrlById(parameters.mapId);
-      console.log('dntp service response: ', mapQuery);
-      if (mapQuery.error !== undefined) {
-        autohostMgrCltNetwork.send2plasmid({
-          'error': 'map not found',
-        });
-        break;
-      }
+  eventEmitter.on('plasmidRequest', async (requestDict)=>{
+    const action=requestDict.action;
+    const parameters=requestDict.parameters;
+    switch (action) {
+      case 'startGame':
+        // check if map exists
+        const mapQuery = await dntpCommunicator.getMapUrlById(parameters.mapId);
+        console.log('dntp service response: ', mapQuery);
+        if (mapQuery.map=='') {
+          autohostMgrCltNetwork.send2plasmid({
+            'action': 'error',
+            'parameters': {
+              'info': 'map File Empty Response',
+            }
+          });
+          break;
+        }
 
-      // required by hoster
 
-      parameters.map = mapQuery.map.map_name;
+        parameters.map = mapQuery.map.map_name;
+        const res = downloadMap(mapQuery, config.localMapDir);
+        if (res === false) {
+          autohostMgrCltNetwork.send2plasmid({
+            'action': 'error',
+            'parameters': {
+              'info': 'map File Download Failure',
+            }
+          });
+          break;
+        }
 
-      const res = downloadMap(mapQuery, config.localMapDir);
-      if (res === false) {
-        autohostMgrCltNetwork.send2plasmid({
-          'error': 'map download failed',
-        });
-        break;
-      }
-
-      try {
-        newRoom(parameters);
-        console.log('startGame');
-      } catch (e) {
-        autohostMgrCltNetwork.send2plasmid(JSON.stringify({
-          'action': 'startGame',
-          'error': 'unknown error',
-        }));
-      }
-    case 'killEngine':
-      killEngine(parameters);
-  }
-});
-
-// helper function
-/**
- *
- * @param {object} parameters
- */
-function newRoom(parameters) {
-  const worker = new Worker('./hoster/hoster.js');
-  const roomID=parameters.id;
-  const title=parameters.title;
-  rooms[parameters.id] = worker;
-  rooms[parameters.id].postMessage({'action': 'startGame', 'parameters': parameters});
-  // hoster to mgr event listener
-  worker.addEventListener('message', (e) => {
-    // see file: lib/autohostInterfaceNetwork.js
-    const message = JSON.parse(e.data);
-    message.parameters.roomID = roomID;
-    message.parameters.title = title;
-    // console.log('autohost interface:');
-    console.log(message);
-    autohostMgrCltNetwork.send2plasmid(message);
-    // seems this variable not used
-
-    // eslint-disable-next-line no-unused-vars
+        try {
+          newRoom(parameters);
+          console.log('startGame');
+        } catch (e) {
+          autohostMgrCltNetwork.send2plasmid(JSON.stringify({
+            'action': 'error',
+            'parameters': {
+              'info': 'room Prototype Init Failure',
+            }
+          }));
+        }
+      case 'killEngine':
+        killEngine(parameters);
+    }
   });
-  rooms[parameters.id] = worker;
+
+  // helper function
+  /**
+   *
+   * @param {object} parameters
+   */
+  function newRoom(parameters) {
+    const worker = new Worker('./hoster/hoster.js');
+    const roomID=parameters.id;
+    const title=parameters.title;
+    rooms[parameters.id] = worker;
+    rooms[parameters.id].postMessage({'action': 'startGame', 'parameters': parameters});
+    // hoster to mgr event listener
+    worker.addEventListener('message', (e) => {
+      // see file: lib/autohostInterfaceNetwork.js
+      const message = JSON.parse(e.data);
+      message.parameters.roomID = roomID;
+      message.parameters.title = title;
+      // console.log('autohost interface:');
+      console.log(message);
+      autohostMgrCltNetwork.send2plasmid(message);
+      // seems this variable not used
+
+      // eslint-disable-next-line no-unused-vars
+    });
+    rooms[parameters.id] = worker;
+  }
+}
+catch(e) {
+  for (room in rooms) {
+    message = {
+      action: 'serverEnding',
+      parameters: {'roomID': room, 'title': [rooms].room.title}
+    };
+    autohostMgrCltNetwork.send2plasmid(message);
 }
 
 /**
